dd3a3a086893a36c060315fa8a7458d8217abaed
Josiah VanderZee <josiah_vanderzee@mediacombb.net>
2018-09-04 19:25:34 -0500

    Update gen.py to select a fork to work in

--- gen.py
+++ gen.py
@@ -1,10 +1,17 @@
 import shutil, os
+import sys
 
 print ( "------------------ GENERATING CMAKE ------------------" )
 
-script_dir = str( os.path.dirname( os.path.realpath( __file__ ) ) )
+userDirName = "qub3d"
 
-root_dir = script_dir + "/../"
+if len(sys.argv) > 1:
+	userDirName = sys.argv[1]
+
+script_dir = str( os.path.dirname( os.path.realpath( __file__ ) ) )
+root_dir = script_dir + "/../" + userDirName + "/"
+print root_dir
+
 cmake_dir = script_dir + "/resources/cmake/"
 
 shutil.copyfile ( cmake_dir + "root.cmake", root_dir + "CMakeLists.txt" )
